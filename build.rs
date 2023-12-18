8e2621cc108f093eeff0b92290cba758859062fb
Aleksandr Mikhailov <iam@fidonode.me>
2023-12-18 20:25:02 +0100

    Fix cargo publish

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/rusty.protocol.proto"], &["src/"])?;
+    let empty_imports: Vec<String> = Vec::default();
+    prost_build::compile_protos(&["src/rusty.protocol.proto"], &empty_imports)?;
     Ok(())
 }
